27967870181f7c448f28f850233c77030e170117
Ax-47 <armingLwnza@gmail.com>
2026-02-24 11:56:04 +0700

    add: db

---
+++ src/main/java/com/example/restservice/config/config.java
@@ -0,0 +1,13 @@
+package com.example.restservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class AppConfig {
+//
+// @Bean
+// public ProductService productService(ProductRepositoryPort port) {
+// return new ProductService(port);
+// }
+// }
