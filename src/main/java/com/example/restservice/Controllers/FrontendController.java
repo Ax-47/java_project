02543f5aa4e25a5da2b55a5e5dadffcf50966000
Ax-47 <armingLwnza@gmail.com>
2026-02-27 01:10:15 +0700

    fix: depency injection

---
+++ src/main/java/com/example/restservice/Controllers/FrontendController.java
@@ -0,0 +1,19 @@
+package com.example.restservice.Controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for the home page.
+ */
+@Controller
+public class FrontendController {
+
+  @GetMapping("/")
+  public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+    return "index";
+  }
+}
