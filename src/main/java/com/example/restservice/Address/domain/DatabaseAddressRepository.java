e472575fb92e1feec3b3220ca123147891c62a91
Ax-47 <armingLwnza@gmail.com>
2026-02-28 17:24:28 +0700

    fix: Id type

--- src/main/java/com/example/restservice/Address/domain/DatabaseAddressRepository.java
+++ src/main/java/com/example/restservice/Address/domain/DatabaseAddressRepository.java
@@ -1,9 +1,12 @@
 package com.example.restservice.Address.domain;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface DatabaseAddressRepository {
-  public Address save(Address address); 
-  Optional<Address> findById(Long id);
+  public Address save(Address address);
+
+  Optional<Address> findById(UUID id);
+
   public Address delete(Address address);
 }
--- src/main/java/com/example/restservice/Address/dto/DeleteAddressRequestDTO.java
+++ src/main/java/com/example/restservice/Address/dto/DeleteAddressRequestDTO.java
@@ -1,12 +1,11 @@
 package com.example.restservice.Address.dto;
 
+import java.util.UUID;
+
 import jakarta.validation.constraints.NotNull;
 
 public record DeleteAddressRequestDTO(
-        @NotNull(message = "Address ID is required")
-        Long addressId,
-
-        @NotNull(message = "User ID is required")
-        Long userId
-) {
+                @NotNull(message = "Address ID is required") UUID addressId,
+                // TODO: delete ts use userId from auth intead of ts.
+                @NotNull(message = "User ID is required") UUID userId) {
 }
