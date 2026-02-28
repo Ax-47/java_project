a85583a5a1d69dc3d957f21bb8d291d6827ac44a
AlexfromArch <firstkalff@gmail.com>
2026-02-28 17:21:15 +0700

    adddeleteinAddressController

--- src/main/java/com/example/restservice/Address/controllers/AddressController.java
+++ src/main/java/com/example/restservice/Address/controllers/AddressController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.*;
 
 import com.example.restservice.Address.dto.CreateAddressRequestDTO;
 import com.example.restservice.Address.dto.CreateAddressResponseDTO;
+import com.example.restservice.Address.dto.DeleteAddressRequestDTO;
+import com.example.restservice.Address.dto.DeleteAddressResponseDTO;
 import com.example.restservice.Address.usecases.CreateAddressUsecase;
+import com.example.restservice.Address.usecases.DeleteAddressUsecase;
 
 import jakarta.validation.Valid;
 
@@ -14,9 +17,11 @@ import jakarta.validation.Valid;
 public class AddressController {
 
   private final CreateAddressUsecase createAddressUsecase;
+  private final DeleteAddressUsecase deleteAddressUsecase;
 
-  public AddressController(CreateAddressUsecase createAddressUsecase) {
+  public AddressController(CreateAddressUsecase createAddressUsecase,DeleteAddressUsecase deleteAddressUsecase) {
     this.createAddressUsecase = createAddressUsecase;
+    this.deleteAddressUsecase = deleteAddressUsecase;
   }
 
   @PostMapping
@@ -24,6 +29,14 @@ public class AddressController {
       @Valid @RequestBody CreateAddressRequestDTO requestModel) {
 
     CreateAddressResponseDTO response = createAddressUsecase.execute(requestModel);
+
+    return ResponseEntity.ok(response);
+  }
+  @DeleteMapping
+  public ResponseEntity<DeleteAddressResponseDTO> delete(
+    @Valid @RequestBody DeleteAddressRequestDTO requestModel) {
+
+    DeleteAddressResponseDTO response = deleteAddressUsecase.execute(requestModel);
 
     return ResponseEntity.ok(response);
   }
