8d6704d6c86aebab489244747a46eaed02be92c0
AlexfromArch <firstkalff@gmail.com>
2026-02-28 17:05:32 +0700

    AddressDeteleImplement

--- src/main/java/com/example/restservice/Address/domain/DatabaseAddressRepository.java
+++ src/main/java/com/example/restservice/Address/domain/DatabaseAddressRepository.java
@@ -1,5 +1,9 @@
 package com.example.restservice.Address.domain;
 
+import java.util.Optional;
+
 public interface DatabaseAddressRepository {
-  public Address save(Address address);  
+  public Address save(Address address); 
+  Optional<Address> findById(Long id);
+  public Address delete(Address address);
 }
---
+++ src/main/java/com/example/restservice/Address/dto/DeleteAddressResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.restservice.Address.dto;
+
+public record DeleteAddressResponseDTO(String massage){
+
+} 
