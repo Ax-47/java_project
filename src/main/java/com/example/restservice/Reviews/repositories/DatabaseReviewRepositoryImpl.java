78dcbe4bc6e30f427568f5613558db7ffcc27f4c
Ax-47 <armingLwnza@gmail.com>
2026-03-10 21:46:01 +0700

    add: review structure

---
+++ src/main/java/com/example/restservice/Reviews/repositories/DatabaseReviewRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.restservice.Reviews.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.restservice.Address.repositories.JpaAddressRepository;
+import com.example.restservice.Reviews.domain.DatabaseReviewRepository;
+
+@Repository
+public class DatabaseReviewRepositoryImpl implements DatabaseReviewRepository {
+  private final JpaAddressRepository jpaAddressRepository;
+
+  public DatabaseReviewRepositoryImpl(JpaAddressRepository jpaAddressRepository) {
+    this.jpaAddressRepository = jpaAddressRepository;
+  }
+}
