128222a9981d7f26fa655f4d31fcb4e9a542af05
TharathepRain <tharathep.pa@kkumail.com>
2026-03-11 00:42:08 +0700

    feat: implement review creation functionality and related DTOs

--- src/main/java/com/example/restservice/Reviews/models/ReviewModel.java
+++ src/main/java/com/example/restservice/Reviews/models/ReviewModel.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.example.restservice.Address.domain.Address;
+import com.example.restservice.Address.models.AddressModel;
 import com.example.restservice.Reviews.domain.Review;
 
 @Entity
@@ -104,4 +106,37 @@ public class ReviewModel {
         model.getCreatedAt(),
         model.getUpdatedAt());
   }
+
+  public Review toDomain() {
+    return Review.rehydrate(
+        this.id,
+        this.productId,
+        this.userId,
+        this.rating,
+        this.comment,
+        this.createdAt,
+        this.updatedAt);
+  }
+
+  public static ReviewModel fromDomain(Review review) {
+        if (review == null) {
+            return null;
+        }
+
+        ReviewModel entity = new ReviewModel();
+
+        if (review.getId() != null) {
+            entity.id = review.getId();
+        }
+
+        entity.userId = review.getUserId();
+        entity.productId = review.getProductId();
+        entity.rating = review.getRating();
+        entity.comment = review.getComment();
+        entity.createdAt = review.getCreatedAt();
+        entity.updatedAt = review.getUpdatedAt();
+
+        return entity;
+    }
 }
+
