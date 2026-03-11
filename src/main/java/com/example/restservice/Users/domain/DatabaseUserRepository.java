cce0a38b2b5104c96320eb1b37f790e444f98cf1
Ax-47 <armingLwnza@gmail.com>
2026-03-12 00:09:51 +0700

    feat: refresh token

--- src/main/java/com/example/restservice/Users/domain/DatabaseUserRepository.java
+++ src/main/java/com/example/restservice/Users/domain/DatabaseUserRepository.java
@@ -1,11 +1,16 @@
 package com.example.restservice.Users.domain;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface DatabaseUserRepository {
   public User save(User user);
 
   public boolean existsByUsername(String username);
 
   public User findUserByUsername(String username);
+
+  public Optional<User> findUserByUserId(UUID userId);
 
   Page<User> findAllUsers(PageQuery pageable);
 }
