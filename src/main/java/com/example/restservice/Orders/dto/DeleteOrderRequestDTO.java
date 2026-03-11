781d139da134caec17de85090aafdb025b98185e
Alexander Sanford <patcharapol.pantawee@gmail.com>
2026-03-11 11:48:31 +0700

    Order product features
    * addressISdefault
    
    * CreateDeleteCancel-Order | CreateDelete-Product
    
    * CreateDeleteCancel-Order | CreateDelete-Product fix: resolve merge conflicts
    
    * fix: type error
    
    ---------
    
    Co-authored-by: AlexfromArch <firstkalff@gmail.com>
    Co-authored-by: Mikhail Akhsakov <85576471+Ax-47@users.noreply.github.com>
    Co-authored-by: Ax-47 <armingLwnza@gmail.com>

---
+++ src/main/java/com/example/restservice/Orders/dto/DeleteOrderRequestDTO.java
@@ -0,0 +1,9 @@
+package com.example.restservice.Orders.dto;
+
+import java.util.UUID;
+import jakarta.validation.constraints.NotNull;
+
+public record DeleteOrderRequestDTO(
+                @NotNull UUID orderId,
+                @NotNull UUID userId) {
+}
