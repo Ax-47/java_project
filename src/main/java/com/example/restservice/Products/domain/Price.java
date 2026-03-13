1569f0d27caf23a8cfce48e3731f58d1aa03b81d
Ax-47 <armingLwnza@gmail.com>
2026-03-13 21:56:43 +0700

    refactor: Product

--- src/main/java/com/example/restservice/Products/domain/Price.java
+++ src/main/java/com/example/restservice/Products/domain/Price.java
@@ -57,10 +57,8 @@ public final class Price {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o)
-      return true;
-    if (!(o instanceof Price))
-      return false;
+    if (this == o) return true;
+    if (!(o instanceof Price)) return false;
     Price price = (Price) o;
     return value.compareTo(price.value) == 0;
   }
