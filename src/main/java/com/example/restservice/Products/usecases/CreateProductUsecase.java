dafc3071c4cd452a400084e0bbafa1a9d9c7f4ed
Ax-47 <armingLwnza@gmail.com>
2026-03-13 21:56:11 +0700

    refactor: Product

--- src/main/java/com/example/restservice/Products/usecases/CreateProductUsecase.java
+++ src/main/java/com/example/restservice/Products/usecases/CreateProductUsecase.java
@@ -17,13 +17,11 @@ public class CreateProductUsecase {
 
   @Transactional
   public CreateProductResponseDTO execute(CreateProductRequestDTO request) {
-    Product newProduct =
-        Product.create(
-            request.id(),
-            request.name(),
-            request.price(),
-            request.description(),
-            request.createdBy());
+    Product newProduct = Product.create(
+        request.name(),
+        request.price(),
+        request.description(),
+        request.createdBy());
 
     productRepository.save(newProduct);
 
