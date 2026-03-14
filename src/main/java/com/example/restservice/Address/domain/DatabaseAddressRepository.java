6ad1405b721e438ce039f66456c6be32664ae650
Alexander Sanford <patcharapol.pantawee@gmail.com>
2026-03-14 17:10:01 +0700

    feat: find update address (#27)
    * feat: find update address
    
    * Change server port from 9001 to 9000
    
    * fix: เพิ่มมาทำควยไร
    
    ---------
    
    Co-authored-by: Mikhail Akhsakov <85576471+Ax-47@users.noreply.github.com>

--- src/main/java/com/example/restservice/Address/domain/DatabaseAddressRepository.java
+++ src/main/java/com/example/restservice/Address/domain/DatabaseAddressRepository.java
@@ -3,6 +3,9 @@ package com.example.restservice.Address.domain;
 import java.util.Optional;
 import java.util.UUID;
 
+import com.example.restservice.common.Page;
+import com.example.restservice.common.PageQuery;
+
 public interface DatabaseAddressRepository {
   public Address save(Address address);
 
@@ -13,4 +16,6 @@ public interface DatabaseAddressRepository {
   public void clearDefaultByUserId(UUID userId);
 
   public void setDefaultAddress(UUID addressId, UUID userId);
+
+  Page<Address> findAllAddresses(PageQuery pageable);
 }
