81db72ad0aa34c82b293487dcbd2555719dc2ac9
Alexander Sanford <patcharapol.pantawee@gmail.com>
2026-03-15 23:44:59 +0700

    Hotfix/dependency (#37)
    * fix: dependency violent
    
    * refactor: format
    
    * fix: polymorphism violent
    
    * refactor: format
    
    * fix: Bl error
    
    * refactor: format
    
    * chore: clean code
    
    * chore: clean code

--- src/main/java/com/example/restservice/TransactionStatements/dto/GetTransactionStatementsByUserResponseDTO.java
+++ src/main/java/com/example/restservice/TransactionStatements/dto/GetTransactionStatementsByUserResponseDTO.java
@@ -4,11 +4,13 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import com.example.restservice.TransactionStatements.domain.TransactionStatements;
+import com.example.restservice.TransactionStatements.domain.TransactionStatement;
 import com.example.restservice.TransactionStatements.domain.TransactionStatementsMethod;
 import com.example.restservice.TransactionStatements.domain.TransactionStatementsStatus;
 import com.example.restservice.TransactionStatements.domain.TransactionStatementsType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record GetTransactionStatementsByUserResponseDTO(
     UUID id,
     UUID userId,
@@ -20,14 +22,14 @@ public record GetTransactionStatementsByUserResponseDTO(
     String referenceId,
     LocalDateTime createdAt) {
 
-  public static GetTransactionStatementsByUserResponseDTO from(TransactionStatements statement) {
+  public static GetTransactionStatementsByUserResponseDTO from(TransactionStatement statement) {
     if (statement == null) return null;
 
     return new GetTransactionStatementsByUserResponseDTO(
         statement.getId(),
         statement.getUserId(),
-        statement.getOrderId(),
-        statement.getAmount(),
+        statement.getOrderId().orElse(null),
+        statement.getAmount().getValue(),
         statement.getType(),
         statement.getMethod(),
         statement.getStatus(),
