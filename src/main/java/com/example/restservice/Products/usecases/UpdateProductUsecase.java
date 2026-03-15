18253a23ad7fa5c776ee04f4291b78ef63ba8bd8
Mikhail Akhsakov <85576471+Ax-47@users.noreply.github.com>
2026-03-15 16:06:13 +0700

    Feature/project (#34) กุให้ทายกุmergeอะไร
    * feat: find product
    
    * feat: upload product
    
    * feat: product category
    
    * feat: product category
    
    * feat: product category untest done
    
    * refactor: format

--- src/main/java/com/example/restservice/Products/usecases/UpdateProductUsecase.java
+++ src/main/java/com/example/restservice/Products/usecases/UpdateProductUsecase.java
@@ -1,9 +1,14 @@
 package com.example.restservice.Products.usecases;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.example.restservice.Images.domain.DatabaseImageRepository;
+import com.example.restservice.Images.domain.Image;
+import com.example.restservice.Images.domain.ImageResource;
+import com.example.restservice.Images.domain.ImageResourceType;
 import com.example.restservice.Products.domain.DatabaseProductRepository;
 import com.example.restservice.Products.domain.Product;
 import com.example.restservice.Products.dto.ProductResponseDTO;
@@ -13,9 +18,13 @@ import com.example.restservice.Products.dto.UpdateProductRequestDTO;
 public class UpdateProductUsecase {
 
   private final DatabaseProductRepository databaseProductRepository;
+  private final DatabaseImageRepository databaseImageRepository;
 
-  public UpdateProductUsecase(DatabaseProductRepository databaseProductRepository) {
+  public UpdateProductUsecase(
+      DatabaseProductRepository databaseProductRepository,
+      DatabaseImageRepository databaseImageRepository) {
     this.databaseProductRepository = databaseProductRepository;
+    this.databaseImageRepository = databaseImageRepository;
   }
 
   public ProductResponseDTO execute(UUID id, UpdateProductRequestDTO request) {
@@ -27,7 +36,9 @@ public class UpdateProductUsecase {
 
     product.update(request.name(), request.price(), request.description());
 
+    List<Image> images =
+        databaseImageRepository.findByResource(ImageResource.of(id, ImageResourceType.PRODUCT));
     databaseProductRepository.save(product);
-    return ProductResponseDTO.from(product);
+    return ProductResponseDTO.from(product, images);
   }
 }
