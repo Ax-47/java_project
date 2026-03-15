1dfad7d9e88e36b4cfb4f27334e1b454cd16db4b
Ax-47 <armingLwnza@gmail.com>
2026-03-15 16:26:39 +0700

    feat: add path to frontend

--- src/main/java/com/example/restservice/Controllers/FrontendController.java
+++ src/main/java/com/example/restservice/Controllers/FrontendController.java
@@ -10,23 +10,80 @@ import com.example.restservice.Auth.dto.UserPrincipalDTO;
 /** Controller for the home page. */
 @Controller
 public class FrontendController {
+  @GetMapping("/")
+  public String index(@AuthenticationPrincipal UserPrincipalDTO user, Model model) {
+    model.addAttribute("user", user);
+    return "index";
+  }
+
+  @GetMapping("/sign-in")
+  public String signin() {
+    return "sign-in";
+  }
+
+  @GetMapping("/sign-up")
+  public String signup() {
+    return "sign-up";
+  }
+
+  /*
+   * =========================
+   * Protected pages
+   * =========================
+   */
 
   @GetMapping("/dashboard")
-  public String dashboard(@AuthenticationPrincipal UserPrincipalDTO userPrincipal, Model model) {
+  public String dashboard(@AuthenticationPrincipal UserPrincipalDTO user, Model model) {
 
-    model.addAttribute("user", userPrincipal);
+    if (user == null) {
+      return "redirect:/sign-in";
+    }
 
+    model.addAttribute("user", user);
     return "dashboard";
   }
 
-  @GetMapping("/signin")
-  public String signin() {
-    return "signin";
+  @GetMapping("/profile")
+  public String profile(@AuthenticationPrincipal UserPrincipalDTO user, Model model) {
+
+    if (user == null) {
+      return "redirect:/sign-in";
+    }
+
+    model.addAttribute("user", user);
+    return "profile";
   }
 
-  @GetMapping("/")
-  public String index(@AuthenticationPrincipal UserPrincipalDTO userPrincipal, Model model) {
-    model.addAttribute("user", userPrincipal);
-    return "index";
+  @GetMapping("/order")
+  public String order(@AuthenticationPrincipal UserPrincipalDTO user, Model model) {
+
+    if (user == null) {
+      return "redirect:/sign-in";
+    }
+
+    model.addAttribute("user", user);
+    return "order";
+  }
+
+  @GetMapping("/address")
+  public String address(@AuthenticationPrincipal UserPrincipalDTO user, Model model) {
+
+    if (user == null) {
+      return "redirect:/sign-in";
+    }
+
+    model.addAttribute("user", user);
+    return "address";
+  }
+
+  @GetMapping("/cash")
+  public String cash(@AuthenticationPrincipal UserPrincipalDTO user, Model model) {
+
+    if (user == null) {
+      return "redirect:/sign-in";
+    }
+
+    model.addAttribute("user", user);
+    return "cash";
   }
 }
