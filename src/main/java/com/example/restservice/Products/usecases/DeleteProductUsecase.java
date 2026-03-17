676eacbb6ec477f5df0bea3d37d3b14d46a6815e
Ax-47 <armingLwnza@gmail.com>
2026-03-17 23:02:06 +0700

    feat: admin page

--- src/main/java/com/example/restservice/Products/usecases/DeleteProductUsecase.java
+++ src/main/java/com/example/restservice/Products/usecases/DeleteProductUsecase.java
@@ -2,31 +2,36 @@ package com.example.restservice.Products.usecases;
 
 import org.springframework.stereotype.Service;
 
+import com.example.restservice.ProductCategories.domain.DatabaseProductCategoryRepository;
 import com.example.restservice.Products.domain.DatabaseProductRepository;
 import com.example.restservice.Products.domain.Product;
 import com.example.restservice.Products.dto.DeleteProductRequestDTO;
 import com.example.restservice.Products.dto.DeleteProductResponseDTO;
 import com.example.restservice.Products.exceptions.ProductNotFoundException;
-import com.example.restservice.Products.exceptions.UnauthorizedProductActionException;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class DeleteProductUsecase {
 
   private final DatabaseProductRepository databaseProductRepository;
+  private final DatabaseProductCategoryRepository databaseProductCategoryRepository;
 
-  public DeleteProductUsecase(DatabaseProductRepository databaseProductRepository) {
+  public DeleteProductUsecase(
+      DatabaseProductCategoryRepository databaseProductCategoryRepository,
+      DatabaseProductRepository databaseProductRepository) {
+    this.databaseProductCategoryRepository = databaseProductCategoryRepository;
     this.databaseProductRepository = databaseProductRepository;
   }
 
+  @Transactional
   public DeleteProductResponseDTO execute(DeleteProductRequestDTO request) {
     Product existingProduct =
         this.databaseProductRepository
             .findById(request.productId())
             .orElseThrow(() -> new ProductNotFoundException("Product not found"));
 
-    if (!existingProduct.getCreatedBy().equals(request.userId())) {
-      throw new UnauthorizedProductActionException("Unauthorized to delete this product");
-    }
+    databaseProductCategoryRepository.deleteByProductId(request.productId());
     this.databaseProductRepository.delete(existingProduct);
 
     return new DeleteProductResponseDTO("Product was deleted successfully");
